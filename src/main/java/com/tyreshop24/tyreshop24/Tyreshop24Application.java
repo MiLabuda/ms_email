5df318d5f1e9186744879f6613dba331fab4f85f
Michał Łabuda <michal.labuda@futurum.tech>
2024-04-17 22:02:19 +0200

    Initial commit

---
+++ src/main/java/com/tyreshop24/tyreshop24/Tyreshop24Application.java
@@ -0,0 +1,13 @@
+package com.tyreshop24.tyreshop24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tyreshop24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tyreshop24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyreshop24/tyreshop24/Tyreshop24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyreshop24.tyreshop24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tyreshop24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
