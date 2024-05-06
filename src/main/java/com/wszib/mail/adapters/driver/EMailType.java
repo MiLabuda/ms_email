5a6ccbcc86db900b0784897ef5c20ba0cda1cbcb
Michał Łabuda <michal.labuda@futurum.tech>
2024-05-06 22:55:38 +0200

    Extend MailRequestMessage & Add mail type enum

---
+++ src/main/java/com/wszib/mail/adapters/driver/EMailType.java
@@ -0,0 +1,7 @@
+package com.wszib.mail.adapters.driver;
+
+public enum EMailType {
+    RESET_PASSWORD,
+    REGISTRATION_CONFIRMATION,
+    CONTACT_US
+}
--- src/main/java/com/wszib/mail/adapters/driver/MailRequestMessage.java
+++ src/main/java/com/wszib/mail/adapters/driver/MailRequestMessage.java
@@ -3,11 +3,14 @@ package com.wszib.mail.adapters.driver;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Email;
 
-public record MailRequest(
+
+public record MailRequestMessage(
         @NotBlank String ownerRef,
         @NotBlank @Email String emailFrom,
         @NotBlank @Email String emailTo,
         @NotBlank String subject,
-        @NotBlank String text
+        @NotBlank String text,
+        @NotBlank String baseUrl,
+        @NotBlank EMailType mailType
 ) {
 }
