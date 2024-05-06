41f60d149def75535c4365a804956da0762ebff3
Michał Łabuda <michal.labuda@futurum.tech>
2024-05-07 00:40:54 +0200

    Implement IdGenerator util

---
+++ src/main/java/com/wszib/mail/hexagon/core/IdGenerator.java
@@ -0,0 +1,11 @@
+package com.wszib.mail.hexagon.core;
+
+import java.util.Random;
+public final class IdGenerator {
+    public static Long generateId() {
+        final long MAX_ID = 900_000_000L;
+        final long RANGE = MAX_ID + 1;
+        Random random = new Random();
+        return (long) (random.nextDouble() * RANGE);
+    }
+}
