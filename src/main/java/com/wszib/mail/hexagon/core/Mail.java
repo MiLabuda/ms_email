ea7c2bd3b7db58aca4065458d03ff3722ffb223f
Michał Łabuda <michal.labuda@futurum.tech>
2024-05-06 23:50:18 +0200

    Extend mail entity

---
+++ src/main/java/com/wszib/mail/hexagon/core/Mail.java
@@ -0,0 +1,29 @@
+package com.wszib.mail.hexagon.core;
+
+import com.wszib.mail.adapters.driver.EMailType;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "mails")
+public class Mail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    private String baseUrl;
+    private ZonedDateTime sendDateEmail;
+    private EMailType mailType;
+    private StatusMail statusMail;
+
+    @Transient
+    private String text;
+}
