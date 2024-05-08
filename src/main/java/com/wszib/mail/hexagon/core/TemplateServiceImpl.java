ccba72924cc638128c88c244a73b5a3ca05de2b6
Michał Łabuda <michal.labuda@futurum.tech>
2024-05-09 00:01:34 +0200

    Implement registration_confirmation mail method

--- src/main/java/com/wszib/mail/hexagon/core/TemplateServiceImpl.java
+++ src/main/java/com/wszib/mail/hexagon/core/TemplateServiceImpl.java
@@ -1,6 +1,7 @@
 package com.wszib.mail.hexagon.core;
 
 
+import com.wszib.mail.config.MailProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
@@ -9,11 +10,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 class TemplateServiceImpl {
 
+    private final MailProperties mailProperties;
+
+    private final String HOST_MAIL = mailProperties.getHostMail();
+
     SimpleMailMessage constructEmail(Mail mail) {
         return switch (mail.getMailType()) {
-            //TODO pass real token
             case RESET_PASSWORD -> constructResetTokenEmail(mail);
-            case REGISTRATION_CONFIRMATION, CONTACT_US -> null;
+            case REGISTRATION_CONFIRMATION -> constructRegistrationConfirmationEmail(mail);
+            case CONTACT_US -> null;
         };
     }
 
@@ -22,12 +27,21 @@ class TemplateServiceImpl {
         String message = "Click on the link below to reset your password. \r\n";
         String subject = "Reset Password";
         String body = message + " \r\n" + url;
+
         mail.setText(body);
         mail.setSubject(subject);
-        //TODO Implement properties table in database as well as a service to retrieve them
-//        email.setFrom(env.getProperty("support.email"));
-        String hostEmail = "support@tyreshop24.com";
-        mail.setEmailFrom(hostEmail);
+        mail.setEmailFrom(HOST_MAIL);
+        return constructSimpleMailMessage(mail);
+    }
+
+    SimpleMailMessage constructRegistrationConfirmationEmail(Mail mail) {
+        String url = mail.getBaseUrl() + "/auth/confirm?token=" + mail.getText();
+        String message = buildEmail(mail.getEmailTo(), url);
+        String subject = "Confirm registration";
+
+        mail.setText(message);
+        mail.setSubject(subject);
+        mail.setEmailFrom(HOST_MAIL);
         return constructSimpleMailMessage(mail);
     }
 
@@ -39,4 +53,75 @@ class TemplateServiceImpl {
         email.setFrom(mail.getEmailFrom());
         return email;
     }
+
+
+    private String buildEmail(String name, String link) {
+        return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
+                "\n" +
+                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
+                "\n" +
+                "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
+                "        \n" +
+                "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                "          <tbody><tr>\n" +
+                "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
+                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td style=\"padding-left:10px\">\n" +
+                "                  \n" +
+                "                    </td>\n" +
+                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
+                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirm your email</span>\n" +
+                "                    </td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "              </a>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "        </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
+                "      <td>\n" +
+                "        \n" +
+                "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
+                "        \n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Thank you for registering. Please click on the below link to activate your account: </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Activate Now</a> </p></blockquote>\n Link will expire in 15 minutes. <p>See you soon</p>" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+                "\n" +
+                "</div></div>";
+    }
+
 }
