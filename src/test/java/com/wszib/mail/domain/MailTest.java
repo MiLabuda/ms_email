da62c259d05ccf0ad555d0d36d088129ee1ad987
Michał Łabuda <mlabuda544@gmail.com>
2024-06-12 23:46:46 +0200

    Temporarily clear tests

---
+++ src/test/java/com/wszib/mail/domain/MailTest.java
@@ -0,0 +1,90 @@
+package com.wszib.mail.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MailTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getEmailFrom() {
+    }
+
+    @Test
+    void getEmailTo() {
+    }
+
+    @Test
+    void getSubject() {
+    }
+
+    @Test
+    void getBaseUrl() {
+    }
+
+    @Test
+    void getCreatedAt() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void getStatus() {
+    }
+
+    @Test
+    void getText() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setEmailFrom() {
+    }
+
+    @Test
+    void setEmailTo() {
+    }
+
+    @Test
+    void setSubject() {
+    }
+
+    @Test
+    void setBaseUrl() {
+    }
+
+    @Test
+    void setCreatedAt() {
+    }
+
+    @Test
+    void setType() {
+    }
+
+    @Test
+    void setStatus() {
+    }
+
+    @Test
+    void setText() {
+    }
+}
